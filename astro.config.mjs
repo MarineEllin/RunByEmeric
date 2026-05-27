34f349519821784f9e5916f7a9276e2ebd296bfa
MarineEllin <marine.ellin@gmail.com>
2026-05-27 16:09:38 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
+  site: "https://runbyemeric.ch",
+  devToolbar: {
     enabled: false,
   },
 });
-
